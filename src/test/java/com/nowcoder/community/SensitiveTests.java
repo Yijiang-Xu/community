e8f73bd7c3b6ec2a5a474d39ed8b827136c62f73
xyijiang <xyijiang@126.com>
2020-05-12 23:16:14 +0800

    3.2 发布帖子

---
+++ src/test/java/com/nowcoder/community/SensitiveTests.java
@@ -0,0 +1,22 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.SensitiveFilter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SensitiveTests {
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter() {
+        String text = "这里可以赌@~!博，可以嫖@!~娼，可以吸毒，可以开票，哈哈哈";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+}
