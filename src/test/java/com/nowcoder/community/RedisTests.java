e454e9c9eaa6fe8e5b40be68292008587070c7a1
xyijiang <xyijiang@126.com>
2020-05-21 22:04:34 +0800

    4.3 点赞功能

---
+++ src/test/java/com/nowcoder/community/RedisTests.java
@@ -0,0 +1,71 @@
+package com.nowcoder.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class RedisTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testStrings() {
+        String redisKey = "test:count";
+
+        redisTemplate.opsForValue().set(redisKey, 1);
+
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+        System.out.println(redisTemplate.opsForValue().decrement(redisKey));
+    }
+
+    @Test
+    public void testKeys() {
+        //redisTemplate.delete("test:count");
+        System.out.println(redisTemplate.hasKey("test:count"));
+        redisTemplate.expire("test:count", 10, TimeUnit.SECONDS);
+    }
+
+    //多次访问通一个key
+    @Test
+    public void testBoundOperation() {
+        String redisKey = "test:count";
+        BoundValueOperations operations = redisTemplate.boundValueOps(redisKey);
+        operations.increment();
+        operations.increment();
+        System.out.println(operations.get());
+    }
+
+    //编程式事务
+    @Test
+    public void testTransactional() {
+        Object obj = redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations redisOperations) throws DataAccessException {
+                String redisKey = "test:tx";
+
+                //启用事务
+                redisOperations.multi();
+                redisOperations.opsForSet().add(redisKey, "zhangsan");
+                redisOperations.opsForSet().add(redisKey, "lisi");
+                redisOperations.opsForSet().add(redisKey, "wangwu");
+
+                System.out.println(redisOperations.opsForSet().members(redisKey));
+                return redisOperations.exec();
+            }
+        });
+
+        System.out.println(obj);
+    }
+}
