4531d75077c920c6a6a1c77c06b5b645e974ddc8
xyijiang <xyijiang@126.com>
2020-06-03 09:58:08 +0800

    7.7 热帖排行

--- src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -17,7 +17,7 @@ public interface DiscussPostMapper {
      * @param limit  每页显示数据的个数
      * @return
      */
-    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit,int orderMode);
 
     //@Param("别名")
     //如果只有一个参数，并且在<if>里使用，必须加别名
@@ -32,5 +32,9 @@ public interface DiscussPostMapper {
     int updateType(int id, int type);
 
     int updateStatus(int id, int status);
+
+    int updateScore(int id,double score);
+
+
 
 }
