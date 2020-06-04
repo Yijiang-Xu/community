e9549c3ae42952351362f7e37f2122d22763149b
xyijiang <xyijiang@126.com>
2020-06-04 10:48:59 +0800

    7.8 生成长图 wkhtmltopdf

--- src/main/java/com/nowcoder/community/event/EventConsumer.java
+++ src/main/java/com/nowcoder/community/event/EventConsumer.java
@@ -8,13 +8,23 @@ import com.nowcoder.community.service.DiscussPostService;
 import com.nowcoder.community.service.ElasticsearchService;
 import com.nowcoder.community.service.MessageService;
 import com.nowcoder.community.util.CommunityConstant;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -32,6 +42,13 @@ public class EventConsumer implements CommunityConstant {
 
     @Autowired
     private ElasticsearchService elasticsearchService;
+
+    @Value("${wk.image.command}")
+    private String wkImageCommand;
+
+    @Value("${wk.image.storage}")
+    private String wkImageStorage;
+
 
     @KafkaListener(topics = {TOPIC_COMMENT, TOPIC_LIKE, TOPIC_FOLLOW})
     public void handleCommentMessage(ConsumerRecord record) {
@@ -103,5 +120,56 @@ public class EventConsumer implements CommunityConstant {
 
         elasticsearchService.deleteDiscussPost(event.getEntityId());
     }
+
+    // 消费分享事件
+    @KafkaListener(topics = TOPIC_SHARE)
+    public void handleShareMessage(ConsumerRecord record) {
+        if (record == null || record.value() == null) {
+            logger.error("消息的内容为空!");
+            return;
+        }
+
+        Event event = JSONObject.parseObject(record.value().toString(), Event.class);
+        if (event == null) {
+            logger.error("消息格式错误!");
+            return;
+        }
+
+        String htmlUrl = (String) event.getData().get("htmlUrl");
+        String fileName = (String) event.getData().get("fileName");
+        String suffix = (String) event.getData().get("suffix");
+
+        String cmd = wkImageCommand + " --quality 75 "
+                + htmlUrl + " " + wkImageStorage + "/" + fileName + suffix;
+        try {
+            Runtime.getRuntime().exec(cmd);
+            logger.info("生产长图成功" + cmd);
+        } catch (IOException e) {
+            logger.error("生成长图失败" + e.getMessage());
+        }
+    }
+
+    // 获取长图
+    @RequestMapping(path = "/share/image/{fileName}", method = RequestMethod.GET)
+    public void getShareImage(@PathVariable("fileName") String fileName, HttpServletResponse response) {
+        if (StringUtils.isBlank(fileName)) {
+            throw new IllegalArgumentException("文件名不能为空！");
+        }
+
+        response.setContentType("image/png");
+        File file = new File(wkImageStorage + "/" + fileName + ".png");
+        try {
+            OutputStream os = response.getOutputStream();
+            FileInputStream fis = new FileInputStream(file);
+            byte[] buffer = new byte[1024];
+            int b = 0;
+            while ((b = fis.read(buffer)) != -1) {
+                os.write(buffer, 0, b);
+            }
+        } catch (IOException e) {
+            logger.error("获取长图失败" + e.getMessage());
+        }
+    }
+
 
 }
--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -35,6 +35,9 @@ public interface CommunityConstant {
     //主题：关注
     String TOPIC_FOLLOW = "follow";
 
+    //主题：分享
+    String TOPIC_SHARE = "share";
+
     //系统用户ID
     int SYSTEM_USER_ID = 1;
 
